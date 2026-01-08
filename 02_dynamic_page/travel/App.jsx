e8fb2d236ecf3eeb4cdd03083b1224a51b0a7c5e
Arghajeet Bhattacharya <arghajeet.bhattacharya@hotmail.com>
2026-01-08 18:24:56 +0530

    optimize dynamic rendering

--- 02_dynamic_page/travel/App.jsx
+++ 02_dynamic_page/travel/App.jsx
@@ -1,20 +1,25 @@
 import Header from "./components/Header";
 import TravelList from "./components/TravelList";
+import dummyTravel from './travel-data';
 
 export default function App() {
+    const travelComponent = dummyTravel.map(element =>
+        <TravelList
+            key={element.id}
+            image={{ src: element.img.src, alt: element.img.alt }}
+            // title={element.title}
+            // country={element.country}
+            // googleMapsLink={element.googleMapsLink}
+            // dates={element.dates}
+            description={element.text}
+            {...element}
+        />
+    )
     return (
         <>
             <Header />
             <main className="container">
-                <TravelList
-                    image={{ src: "https://scrimba.com/links/travel-journal-japan-image-url", alt: "Mount Fuji" }}
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink= "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                />
-
+                {travelComponent}
             </main>
         </>
     )
