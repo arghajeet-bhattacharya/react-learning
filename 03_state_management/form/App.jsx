1bccf1dd9b167a4ffaf9386856ba7b8f1f61fdeb
Arghajeet Bhattacharya <arghajeet.bhattacharya@hotmail.com>
2026-02-22 00:03:23 +0530

    form option

--- 03_state_management/form/App.jsx
+++ 03_state_management/form/App.jsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom/client';
 export default function App() {
 
     const handleFormSubmit = (formData) => {
-        console.log('formData: ', formData)
-        const email = formData.get('email-input');
-        const password = formData.get('password-input');
+        const data = Object.fromEntries(formData);
+        const skillSet = formData.getAll('skillSet');
+        const finalDta = { ...data, skillSet }
+        console.log('finalDta: ', finalDta)
     }
 
     return (
@@ -27,15 +28,36 @@ export default function App() {
                 <fieldset>
                     <legend>Joining Status</legend>
                     <label>
-                        <input type='radio' name='joiningStatus' value='yes' defaultChecked={true}/>Yes
+                        <input type='radio' name='joiningStatus' value='yes' defaultChecked={true} />Yes
                     </label>
                     <label>
-                        <input type='radio' name='joiningStatus'value='no'/>No
+                        <input type='radio' name='joiningStatus' value='no' />No
                     </label>
                     <label>
-                        <input type='radio' name='joiningStatus' value='dont-know'/>Don't know
+                        <input type='radio' name='joiningStatus' value='dont-know' />Don't know
                     </label>
                 </fieldset>
+
+                <fieldset>
+                    <legend>Skill</legend>
+                    <label>
+                        <input type='checkbox' name='skillSet' value='node' defaultChecked={true} />Node.js
+                    </label>
+                    <label>
+                        <input type='checkbox' name='skillSet' value='react' defaultChecked={true} />React.js
+                    </label>
+                    <label>
+                        <input type='checkbox' name='skillSet' value='mysql' />MySql
+                    </label>
+                </fieldset>
+
+                <label>What is your current city</label>
+                <select id='curCity' name='curCity' defaultValue='' required={false}>
+                    <option value='' disabled>-- Choose Option --</option>
+                    <option value='delhi'>Delhi</option>
+                    <option value='hyderabad'>Hyderabad</option>
+                    <option value='bangalore'>Bangalore</option>
+                </select>
 
                 <button>Submit</button>
 
